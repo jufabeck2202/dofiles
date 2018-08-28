118b0d1a6c08437df3f3ad39e0b71d6ded47583b
Julian Beck <ju-fa-beck@t-online.de>
2018-08-28 09:35:10 +0900

    Added npm.sh with a list of useful global packages

---
+++ npm.sh
@@ -0,0 +1,22 @@
+# working with npm
+npm install -g yarn
+npm install -g np
+npm install -g npm-name-cli
+
+# debugging
+npm install -g ndb
+npm install -g node-inspector
+npm install -g nodemon
+
+# general utilities
+npm install -g now
+npm install -g heroku
+npm install -g spoof
+npm install -g vtop
+npm install -g vitomuci
+npm install -g yo
+
+# react
+npm install -g create-react-app
+npm install -g react-native-cli
+
