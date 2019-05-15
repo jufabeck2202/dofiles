d611a3a06fb6a16dc9d00a04c86af99ae24a2d3b
Julian Beck <ju-fa-beck@t-online.de>
2019-05-15 10:19:46 +0200

    added apt packages

--- install/apt.sh
+++ install/apt.sh
@@ -22,6 +22,7 @@ sudo apt install -y \
     unrar \
     hub \
     tmux \
+    openssh-server \
 
 # Pass any argument to this script for a "full" install
 # Installes useful command applications
@@ -46,6 +47,15 @@ if [ "$1" = "full" ]; then
         openvpn \
         network-manager-openvpn \
         network-manager-openvpn-gnome
+
+    # install Jdownloader
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6A68F637 
+    sudo apt-get install -y jdownloader-installer 
+
+    # remote Desktop
+    sudo add-apt-repository ppa:x2go/stable
+    sudo apt-get update
+    sudo apt-get install -y x2goserver x2goserver-xsession
 fi
 
 # Cleanup
